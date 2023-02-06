9d1b4bad82c2aff9164e01f01412fa78caad3e4a
RaozyMainty <razakasontoky18@gmail.com>
2023-02-06 18:29:05 +0300

    feat:accueil

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import { socket, WebSocketProvider } from './WebSocketContexts'
 import { Home } from './components/home'
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Accueil from './components/Accueil';
+
 function App() {
 
   return (
-    <WebSocketProvider value={socket}>
-      <Home></Home>
-    </WebSocketProvider>
+    <>
+      <WebSocketProvider value={socket}>
+        <Home></Home>
+      </WebSocketProvider><div className='App'>
+          <BrowserRouter>
+            <Routes>
+              <Route>
+                <Route path='/accueil' element={<Accueil />} />
+              </Route>
+            </Routes>
+          </BrowserRouter>
+        </div>
+      </>
   )
 }
 
